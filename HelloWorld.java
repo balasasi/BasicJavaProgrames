c06f2c299fe80d7de7a9c182e6040f0922a5e1d2
BalaMurugan <93832424+balasasi@users.noreply.github.com>
2025-05-24 15:54:20 +0530

    Update and rename HelloWorld to HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,9 @@
 //1.Hello Word
 //------------
-class HelloWorld {
-    public static void main(String[] args) {
+class HelloWorld 
+{
+    public static void main(String[] args) 
+    {
         System.out.println("Hello World");
     }
 }
